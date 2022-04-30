2330fc5921f52e37570f151e3a6282eee356cad4
Joshua Baker <thepotatoprogrammer@gmail.com>
2022-04-30 13:57:21 -0500

    Setting up new project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Joshua Baker on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
